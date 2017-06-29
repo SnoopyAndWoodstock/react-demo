e07d5210f2285d1db170135abb0db8333d0f5770
yeren0914 <429441861@qq.com>
2017-06-29 16:33:03 +0800

    comment edit

--- src/containers/comment/comment.component.jsx
+++ src/containers/comment/comment.component.jsx
@@ -4,22 +4,61 @@ import commentState from './comment.state.js';
 import { List,Modal, TextareaItem,InputItem,Button,WingBlank , WhiteSpace, Toast } from 'antd-mobile';
 import './comment.scss'
 @observer
+
 /* input */
 class CommentInput extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    bindName(txt){
+        commentState.setUserName(txt);
+    }
+    bindContent(txt){
+        commentState.setContent(txt);
+    }
+
+    /* 验证 */
+    postData(){
+        if(commentState.state.username== "" || commentState.state.username==undefined){
+            commentState.setTipMsg('请输入名称');
+            commentState.setModalShow(true);
+            return 
+        }
+        if(commentState.state.content =="" || commentState.state.content ==undefined){
+            commentState.setTipMsg('请输入评论内容');
+            commentState.setModalShow(true);
+            return 
+        }
+        if(this.props.onSubmit){
+            const {username,content }=commentState.state;
+            this.props.onSubmit({username,content})
+        }
+        commentState.setContent();
+    }
+
+    closeAlert =key =>() =>{
+        commentState.setModalShow(false)
+    }
+
     render(){
         return(
            <div>
                 <div className="inpit-box">
                     <WhiteSpace />
-                    <InputItem clear placeholder="请输入名称">姓名</InputItem>
-                    <TextareaItem title="内容" placeholder="请输入评论内容" autoHeight >
+                    <InputItem clear placeholder="请输入名称"  onChange={this.bindName.bind(this)} >姓名</InputItem>
+                    <TextareaItem title="内容" placeholder="请输入评论内容" autoHeight onChange={this.bindContent.bind(this)}>
                     </TextareaItem>
                 </div>
                 <WhiteSpace size="lg" />
                 <WingBlank >
-                    <Button className="btn" type="primary">提交评论</Button>
+                    <Button className="btn" type="primary" onClick={this.postData.bind(this)}>提交评论</Button>
                 </WingBlank>
                 <WhiteSpace size="lg" />
+                <Modal title="提示信息" transparent visible={commentState.state.modalShow} onClose={this.closeAlert('modalShow')}
+                footer={[{text:'确定',onPress: ()=> {this.closeAlert('modalShow')();}}]}>
+                    {commentState.state.tipMsg}
+                </Modal>
            </div> 
         )
     }
@@ -27,27 +66,52 @@ class CommentInput extends React.Component{
 
 /* List */
 class CommentList extends React.Component{
+    static defaultProps ={
+        coments:[]
+    }
+    constructor(props){
+        super(props)
+    }
     render() {
+        let commentData =this.props.comments;
         return (
             <div>
-                <Comment />
+                {
+                    commentData.length >0 ? <Comment commentData={commentData} />  :
+                    <div className="nodata">
+                        暂无数据
+                    </div>
+                }
             </div>
         );
     }
 }
 
 class Comment extends React.Component{
+    constructor(props){
+        super(props) 
+    }
     render() {
+        let commentInfo=this.props.commentData;
         return (
-            <div className="comment-list">
-                <WingBlank>   
-                <div className="comment-item">
-                    <label>AA：</label>
-                    <p>嘻嘻哈哈哈输入的内容</p>
-                    <span className="item-time">1分钟前</span>
-                    <span className="item-delete">删除</span>
-                </div>
-                </WingBlank>
+            <div>
+                {
+                   this.props.commentData.map(function(item,index){
+                        return(
+                            <div className="comment-list">
+                                <WingBlank>   
+                                <div className="comment-item">
+                                    <label>{item.username}：</label>
+                                    <p>{item.content}</p>
+                                    <span className="item-time">1分钟前</span>
+                                    <span className="item-delete">删除</span>
+                                </div>
+                                </WingBlank>
+                                <WhiteSpace size="lg" />
+                            </div>
+                        )
+                    }) 
+                }
             </div>
         );
     }
@@ -55,11 +119,23 @@ class Comment extends React.Component{
 
 /* app */
 class CommentApp extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            comments:[]
+        }
+    }
+    handleSubmit(comment){
+        this.state.comments.push(comment);
+        this.setState({
+            comments:this.state.comments
+        })
+    }
     render(){
         return(
             <div className="comment">
-                <CommentInput />
-                <CommentList />
+                <CommentInput  onSubmit={this.handleSubmit.bind(this)} />
+                <CommentList comments={this.state.comments} />
             </div>
         )
     }
--- src/containers/comment/comment.state.js
+++ src/containers/comment/comment.state.js
@@ -7,15 +7,34 @@ class commentState {
 
 	@observable state = {
 		username: '', //用户名
-		content: '' //评论内容
+		content: '', //评论内容
+		tipMsg: '', //提示信息
+		modalShow: false, //是否显示弹窗
+		isData: false //是否有数据
 	};
 
-	@action setName(val) {
+	@action setUserName(val) {
 		this.state.username = val;
 	}
 
 	@action setContent(txt) {
-		this.state.content = txt;
+		if (!!txt) {
+			this.state.content = txt;
+		} else {
+			this.state.content = '';
+		}
+	}
+
+	@action setTipMsg(txt) {
+		this.state.tipMsg = txt;
+	}
+
+	@action setModalShow(b) {
+		this.state.modalShow = b;
+	}
+
+	@action setIsData(b) {
+		this.state.isData = b;
 	}
 
 }
